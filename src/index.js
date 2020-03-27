fef6603d859099f793dcf45cd38c4da07af306d7
Carley Tripp <carley.tripp@outlook.com>
2020-03-27 22:38:31 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   let input = document.getElementById("new-task-description").value;
 
-
+  // on submit, add a list element to #list#tasks equal to input
 });
