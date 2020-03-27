cf9ff087d77f0f4f424ef4889f694a3156f51783
Carley Tripp <carley.tripp@outlook.com>
2020-03-27 23:17:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let input = document.getElementById("new-task-description").value;
   let tasks = document.getElementById("tasks");
   let task = document.createElement("li");
 
   document.getElementById("submit").addEventListener("click", function(event) {
-    console.log(input);
+    let input = document.getElementById("new-task-description").value;
     event.preventDefault();
+    console.log(input);
     // task.innerText = input;
     tasks.appendChild(task);
   });
